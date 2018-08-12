bb2f442f8202f5e2a102b677c1b8ff6a147777b4
Natalie Groeneboom <nataliegroeneboom@Natalies-MacBook-Air.local>
2018-08-12 09:01:56 +0100

    commit

--- server/server.js
+++ server/server.js
@@ -6,9 +6,9 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-// app.get('*', (req, res)=>{
-//   res.sendFile(path.join(publicPath, 'index.html'));
-// });
+app.get('*', (req, res)=>{
+  res.sendFile(path.resolve(publicPath, 'index.html'));
+});
 
 app.listen(port, ()=>{
   console.log('Server is Up');
